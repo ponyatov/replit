e7e0419645bd834a6b797c8ae63164ecce5bf527
Dmitry Ponyatov <ponyatov@meteo.local>
2020-07-22 15:17:45 +0400

    Signed-off-by: Dmitry Ponyatov <ponyatov@meteo.local>

---
+++ main.py
@@ -0,0 +1,1 @@
+from metaL import *
---
+++ metaL.py
@@ -0,0 +1,11 @@
+MODULE = 'metaL'
+TITLE = '[meta]programming [L]anguage'
+ABOUT = 'homoiconic metaprogramming system'
+AUTHOR = 'Dmitry Ponyatov'
+EMAIL = 'dponyatov@gmail.com'
+YEAR = 2020
+LICENSE = 'MIT'
+
+import os, sys
+
+from xxhash import xxh32
