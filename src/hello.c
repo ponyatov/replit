8658d69a1862251193f1f8f9da483c82a6ed6472
Dmitry Ponyatov <dponyatov@gmail.com>
2020-07-22 16:30:54 +0000

    + args

--- src/hello.c
+++ src/hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello World\n");
+  printf("\nHello World\n\n");
+  for (int i=0;i<argc;i++) printf("argv[%i]=%s\n",i,argv[i]);
+  printf("\n");
   return 0;
 }
